a064018da8f9848b269b15d7b5e3020d28f2083c
learn <eric.desimone@cortland.edu>
2019-02-04 10:35:20 -0800

    setting up initial react view

--- client/src/App.js
+++ client/src/App.js
@@ -1,25 +1,16 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import TweetFeed from './components/TweetFeed'
+import { FaTwitter } from "react-icons/fa";
 import './App.css';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
+        <header className="fwitter-header">
+          <h1>THIS IS FWITTER <FaTwitter/></h1>
         </header>
+        <TweetFeed/>
       </div>
     );
   }
---
+++ client/src/components/TweetFeed.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+
+class TweetFeed extends Component {
+
+  render(){
+    return (
+      <div className="the-feed">
+      hello
+      HEELLLOOOOOOOOOOOOOO
+      </div>
+    )
+  }
+
+}
+
+export default TweetFeed
